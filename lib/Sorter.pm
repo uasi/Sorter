b8269eba54176f2f55dfe0bbcf018cfae6fe8f7c
Tomoki Aonuma <uasi@99cm.org>
2011-08-13 03:35:34 +0900

    Pass tests by utilizing built-in sort function

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,29 @@
-# implement this
+package Sorter;
+
+use 5.012;
+use warnings;
+
+sub new {
+    my ($class) = @_;
+    bless {values => []}, $class;
+}
+
+sub get_values {
+    my ($self) = @_;
+    @{$self->{values}};
+}
+
+sub set_values {
+    my ($self, @values) = @_;
+    $self->{values} = \@values;
+}
+
+sub sort {
+    my ($self) = @_;
+    my $values = $self->{values};
+
+    # XXX Implement quicksort
+    @$values = sort { $a <=> $b } @$values;
+}
+
+1;
